01c8a71868ff324fd37986f4852f94a65c1e9220
js.lozano@globant.com <142308Jslp>
2022-04-22 15:16:19 -0500

    First exercise css selectors completed

---
+++ src/main/java/taller2/HerokuApp.java
@@ -0,0 +1,18 @@
+package taller2;
+
+import org.openqa.selenium.By;
+
+public class HerokuApp {
+    String authorLink = "By.cssSelector(.container address:nth-child(6) a:first-child)";
+    String firstTableFirstText = "By.cssSelector(.container ul li:nth-child(3))";
+    String menuListFirstRowText = "By.cssSelector(.container menu li:first-child)";
+    String dirListThirdRowText = "By.cssSelector(.container dir li:last-child)";
+    String formButton = "By.cssSelector(.container form button)";
+    String formReset2Button = "By.cssSelector(.container form input.btn.reset2)";
+    String thirdOptionInSelectElement =
+            "By.cssSelector(.container select[name='select2'] option:last-child)";
+    String firstTableIcelandRowLandAreaColumn =
+            "By.cssSelector(.container table[summary] tbody tr:nth-child(4) td:last-child)";
+    // hyphenation title
+    String javascriptTitle = "By.cssSelector(.container p.limited.hyphens + h3)";
+}
